fcde16bdc8ccb1c5b0aae96fce8ee8352a011067
shikharshikharshikhar <34400575+shikharshikharshikhar@users.noreply.github.com>
2025-08-14 19:25:37 -0400

    Update places.js

--- public/places.js
+++ public/places.js
@@ -1,33 +1,57 @@
-const addPlace = async () => {
-    const label = document.querySelector("#label").value;
-    const address = document.querySelector("#address").value;
-    await axios.put('/places', { label: label, address: address });
-    await loadPlaces();
-}
+doctype html 
+html
+    head 
+        title Places 
+        meta(name="viewport", content="width=device-width,initial-scale=1")
+        script(src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
+        link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",crossorigin="anonymous")
+        // Leaflet CSS
+        link(rel="stylesheet", href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css")
+        style.
+            #map-container { 
+                position: relative; 
+                height: 700px; 
+                margin-bottom: 5rem; 
+            } 
+            #map { 
+                position: absolute; 
+                top: 0; 
+                bottom: 0; 
+                width: 100%; 
+            }
+            tr { 
+                cursor: pointer; 
+            }
+    body 
+        .container 
+            h1.mt-3 Places 
 
-const deletePlace = async (id) => {
-    await axios.delete(`/places/${id}`);
-    await loadPlaces();
-}
+            .row
+                .col-md-6
+                    form.form
+                        .mb-3 
+                            label(for="label") Label 
+                            input#label.form-control(type='text')
+                        .mb-3 
+                            label(for="address") Address 
+                            input#address.form-control(type='text')
+                        button.btn.btn-primary(type='button', onclick='addPlace()') Add Place
 
-const loadPlaces = async () => {
-    const response = await axios.get('/places');
-    const tbody = document.querySelector('tbody');
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
+                    table.table.table-hover
+                        thead   
+                            tr  
+                                th Label 
+                                th Address 
+                                th 
+                        tbody
+                
+                .col-md-6
+                    #map-container
+                        #map
 
-    if (response && response.data && response.data.places) {
-        for (const place of response.data.places) {
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-                <td>${place.label}</td>
-                <td>${place.address}</td>
-                <td>
-                    <button class='btn btn-danger' onclick='deletePlace(${place.id})'>Delete</button>
-                </td>
-            `;
-            tbody.appendChild(tr);
-        }
-    }
-}
+        script(src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM", crossorigin="anonymous")
+        // Leaflet JS - must come before places.js
+        script(src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js")
+        script(src="/places.js")
+        script. 
+            loadPlaces()
